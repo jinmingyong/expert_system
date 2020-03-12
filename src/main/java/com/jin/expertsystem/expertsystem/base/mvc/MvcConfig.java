e82cff1f0ae8f29fd7e4e09d07bcc867e7989d21
jinmingyong <1060854946@qq.com>
2020-03-12 15:43:16 +0800

    删除权限限制

--- src/main/java/com/jin/expertsystem/expertsystem/base/mvc/MvcConfig.java
+++ src/main/java/com/jin/expertsystem/expertsystem/base/mvc/MvcConfig.java
@@ -11,8 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
@@ -59,7 +57,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
         exceptionResolvers.add((HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) -> {
                     Result result = new Result();
-                    if (handler instanceof HandlerMethod) {
+                  /*  if (handler instanceof HandlerMethod) {
                         HandlerMethod handlerMethod = (HandlerMethod) handler;
                         if (e instanceof AuthenticationException) {
                             //密码错误
@@ -79,7 +77,7 @@ public class MvcConfig implements WebMvcConfigurer {
                                     e.getMessage());
                             log.error(message, e);
                         }
-                    } else {
+                    } else {*/
                         if (e instanceof NoHandlerFoundException) {
                             result.setCode(ResultCode.NOT_FOUND.code);
                             result.setMsg("接口 [" + request.getRequestURI() + "] 不存在");
@@ -88,7 +86,7 @@ public class MvcConfig implements WebMvcConfigurer {
                             result.setMsg(e.getMessage());
                             log.error(e.getMessage(), e);
                         }
-                    }
+             //       }
                     responseResult(response, result);
                     return new ModelAndView();
                 }
